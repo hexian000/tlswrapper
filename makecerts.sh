f54672892253f6acd901d673b2cdb591b5f62aec
hexian000 <hexian000@outlook.com>
2021-10-18 18:24:14 +0800

    certs

--- makecerts.sh
+++ makecerts.sh
@@ -1,15 +1,31 @@
-#!/bin/sh -eu
+#!/bin/sh -e
 
-if [ $# -lt 1 ]; then
+if [ -z "$SSLNAME" ]; then
     SSLNAME="example.com"
-else
-    SSLNAME="$1"
 fi
 
-openssl req -newkey rsa:8192 \
-    -new -nodes -x509 \
-    -days 36500 \
-    -out cert.pem \
-    -keyout key.pem \
-    -subj "/C=US/ST=California/L=Mountain View/O=Your Organization/OU=Your Unit/CN=${SSLNAME}" \
-    -addext "subjectAltName = DNS:${SSLNAME}"
+if [ ! -f ca-key.pem ]; then
+    openssl genrsa 8192 >ca-key.pem
+    openssl req -new -x509 -nodes -days 36500 \
+        -key ca-key.pem \
+        -out ca-cert.pem \
+        -subj "/C=US/ST=California/L=Mountain View/O=Your Organization/OU=Your Unit/CN=${SSLNAME}" \
+        -addext "subjectAltName = DNS:${SSLNAME}"
+fi
+
+while [ -n "$1" ]; do
+    NAME="$1"
+
+    openssl req -newkey rsa:8192 -nodes -days 36500 \
+        -keyout "${NAME}-key.pem" \
+        -out "${NAME}.csr" \
+        -subj "/C=US/ST=California/L=Mountain View/O=Your Organization/OU=Your Unit/CN=${SSLNAME}" \
+        -addext "subjectAltName = DNS:${SSLNAME}"
+
+    openssl x509 -req -days 36500 -CAcreateserial \
+        -in "${NAME}.csr" \
+        -out "${NAME}-cert.pem" \
+        -CA ca-cert.pem \
+        -CAkey ca-key.pem
+    shift
+done
