62b5faa41c6b6e89494bb763b7a4af702498472f
hexian000 <hexian000@outlook.com>
2021-12-04 18:36:32 +0800

    remove local test scripts

--- t.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -eux
-
-
-GOOS="linux" GOARCH="arm" GOARM=7 nice go build -trimpath -ldflags "-X main.version=dev-$(date -Iseconds)" -o build/tlswrapper.linux-armv7
-scp build/tlswrapper.linux-armv7 rpi:~/.local/bin/
-ssh rpi -- "mv ~/.local/bin/tlswrapper.linux-armv7 ~/.local/bin/tlswrapper"
