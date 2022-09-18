1eb5cb0cacb569106ff697271f9fced6982d0c77
hexian000 <hexian000@outlook.com>
2022-09-18 10:55:19 +0800

    fix deadlock

--- forwarder/forwarder.go
+++ forwarder/forwarder.go
@@ -11,54 +11,57 @@ import (
 type Forwarder struct {
 	wg      sync.WaitGroup
 	mu      sync.Mutex
-	closers map[io.Closer]io.Closer
+	closers map[io.Closer]struct{}
 }
 
 func New() *Forwarder {
-	return &Forwarder{closers: make(map[io.Closer]io.Closer)}
+	return &Forwarder{closers: make(map[io.Closer]struct{})}
 }
 
 func (f *Forwarder) copyConn(dst net.Conn, src net.Conn) {
 	defer f.wg.Done()
 	defer func() {
-		_ = src.Close()
-		_ = dst.Close()
-	}()
-	defer func() {
 		if err := recover(); err != nil {
 			slog.Errorf("forwarder: %v", err)
 		}
-		f.mu.Lock()
-		defer f.mu.Unlock()
-		delete(f.closers, src)
-	}()
-	func() {
-		f.mu.Lock()
-		defer f.mu.Unlock()
-		f.closers[src] = dst
+		f.close(src)
+		f.close(dst)
 	}()
 	_, _ = io.Copy(dst, src)
 }
 
+func (f *Forwarder) addCloser(c io.Closer) {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+	f.closers[c] = struct{}{}
+}
+
+func (f *Forwarder) close(c io.Closer) {
+	_ = c.Close()
+	f.mu.Lock()
+	defer f.mu.Unlock()
+	delete(f.closers, c)
+}
+
 func (f *Forwarder) Forward(accepted net.Conn, dialed net.Conn) {
+	f.addCloser(accepted)
+	f.addCloser(dialed)
 	f.wg.Add(2)
 	go f.copyConn(accepted, dialed)
 	go f.copyConn(dialed, accepted)
 }
 
-func (f *Forwarder) NumForwards() int {
-	f.mu.Lock()
-	defer f.mu.Unlock()
-	return len(f.closers)
-}
-
 func (f *Forwarder) Close() {
-	f.mu.Lock()
-	defer f.mu.Unlock()
-	for src, dst := range f.closers {
-		_ = src.Close()
-		_ = dst.Close()
-		delete(f.closers, src)
+	closeList := make([]io.Closer, 0)
+	func() {
+		f.mu.Lock()
+		defer f.mu.Unlock()
+		for c := range f.closers {
+			closeList = append(closeList, c)
+		}
+	}()
+	for _, c := range closeList {
+		f.close(c)
 	}
 	f.wg.Wait()
 }
