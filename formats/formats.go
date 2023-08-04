e85d2914f2296b1a5f9db5675649877d02031c72
hexian000 <hexian000@outlook.com>
2023-08-04 15:31:32 +0800

    formats: add SI prefix

--- formats/formats.go
+++ formats/formats.go
@@ -6,16 +6,53 @@ import (
 	"time"
 )
 
+func isNormal(f float64) bool {
+	return !math.IsNaN(f) && !math.IsInf(f, 0) && f != 0.0
+}
+
+var siPrefixPos = [...]string{
+	"k", "M", "G", "T", "P", "E", "Z", "Y", "R", "Q",
+}
+
+var siPrefixNeg = [...]string{
+	"m", "μ", "n", "p", "f", "a", "z", "y", "r", "q",
+}
+
+func SIPrefix(value float64) string {
+	if !isNormal(value) {
+		return fmt.Sprintf("%.0f", value)
+	}
+	e := int(math.Floor(math.Log10(value) / 3.0))
+	if e == 0 {
+		return fmt.Sprintf("%.6g", value)
+	}
+	if e < 0 {
+		i := -e
+		if i > len(siPrefixNeg) {
+			i = len(siPrefixNeg)
+		}
+		v := value / math.Pow(10.0, -3.0*float64(i))
+		prefix := siPrefixNeg[i-1]
+		return fmt.Sprintf("%.6g%s", v, prefix)
+	}
+	i := e
+	if i > len(siPrefixPos) {
+		i = len(siPrefixPos)
+	}
+	v := value / math.Pow(10.0, 3.0*float64(i))
+	prefix := siPrefixPos[i-1]
+	return fmt.Sprintf("%.6g%s", v, prefix)
+}
+
 var iecUnits = [...]string{
 	"B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB",
 }
 
-func isFinite(f float64) bool {
-	return !math.IsNaN(f - f)
-}
-
 func IECBytes(value float64) string {
-	if !isFinite(value) || value < 8192.0 {
+	if !isNormal(value) {
+		return fmt.Sprintf("%.0f", value)
+	}
+	if value < 8192.0 {
 		return fmt.Sprintf("%.0f %s", value, iecUnits[0])
 	}
 	n := (int(math.Log2(value)) - 3) / 10
@@ -24,10 +61,10 @@ func IECBytes(value float64) string {
 	}
 	v := math.Ldexp(value, n*-10)
 	if v < 10.0 {
-		return fmt.Sprintf("%.02f %s", v, iecUnits[n])
+		return fmt.Sprintf("%.2f %s", v, iecUnits[n])
 	}
 	if v < 100.0 {
-		return fmt.Sprintf("%.01f %s", v, iecUnits[n])
+		return fmt.Sprintf("%.1f %s", v, iecUnits[n])
 	}
 	return fmt.Sprintf("%.0f %s", v, iecUnits[n])
 }
@@ -150,24 +187,24 @@ func Duration(d time.Duration) string {
 		if minute >= 10 {
 			return fmt.Sprintf("%s%d:%02.0f", sign, minute, seconds)
 		}
-		return fmt.Sprintf("%s%d:%04.01f", sign, minute, seconds)
+		return fmt.Sprintf("%s%d:%04.1f", sign, minute, seconds)
 	}
 	if second != 0 {
 		if second >= 10 {
 			seconds := float64(second) + float64(milli)*1e-3 + float64(micro)*1e-6 + float64(nano)*1e-9
-			return fmt.Sprintf("%s%.02fs", sign, seconds)
+			return fmt.Sprintf("%s%.2fs", sign, seconds)
 		}
 		millis := float64(second)*1e+3 + float64(milli) + float64(micro)*1e-3 + float64(nano)*1e-6
-		return fmt.Sprintf("%s%03.0fms", sign, millis)
+		return fmt.Sprintf("%s%.0fms", sign, millis)
 	}
 	if milli != 0 {
 		if milli >= 100 {
 			millis := float64(milli) + float64(micro)*1e-3 + float64(nano)*1e-6
-			return fmt.Sprintf("%s%.01fms", sign, millis)
+			return fmt.Sprintf("%s%.1fms", sign, millis)
 		}
 		if milli >= 10 {
 			millis := float64(milli) + float64(micro)*1e-3 + float64(nano)*1e-6
-			return fmt.Sprintf("%s%.02fms", sign, millis)
+			return fmt.Sprintf("%s%.2fms", sign, millis)
 		}
 		micros := float64(milli)*1e+3 + float64(micro) + float64(nano)*1e-3
 		return fmt.Sprintf("%s%.0fµs", sign, micros)
@@ -175,14 +212,17 @@ func Duration(d time.Duration) string {
 	if micro != 0 {
 		if micro >= 100 {
 			micros := float64(micro) + float64(nano)*1e-3
-			return fmt.Sprintf("%s%.01fµs", sign, micros)
+			return fmt.Sprintf("%s%.1fµs", sign, micros)
 		}
 		if micro >= 10 {
 			micros := float64(micro) + float64(nano)*1e-3
-			return fmt.Sprintf("%s%.02fµs", sign, micros)
+			return fmt.Sprintf("%s%.2fµs", sign, micros)
 		}
 		nanos := float64(micro)*1e+3 + float64(nano)
 		return fmt.Sprintf("%s%.0fns", sign, nanos)
 	}
-	return fmt.Sprintf("%s%dns", sign, nano)
+	if nano != 0 {
+		return fmt.Sprintf("%s%dns", sign, nano)
+	}
+	return fmt.Sprintf("%s0", sign)
 }
