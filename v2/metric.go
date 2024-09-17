ab5707852ae82da888c0884a680f7ccaf602790c
He Xian <hexian000@outlook.com>
2024-09-17 13:15:10 +0800

    stats format
    Signed-off-by: He Xian <hexian000@outlook.com>

--- v2/metric.go
+++ v2/metric.go
@@ -227,7 +227,7 @@ func (h *apiStatsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			if t.NumSessions > 0 {
 				s = fmt.Sprintf("%d (%d streams)", t.NumSessions, t.NumStreams)
 			}
-			fprintf(w, "%-20q: %s, since %s\n", t.Name, s, t.LastChanged.Format(time.RFC3339))
+			fprintf(w, "%-20q: %s, %s\n", t.Name, t.LastChanged.Format(time.RFC3339), s)
 		} else {
 			fprintf(w, "%-20q: never seen\n", t.Name)
 		}
