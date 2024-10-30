2bc9b492c45b6e0000b5e886135c3f6b71ebf580
He Xian <hexian000@outlook.com>
2024-10-30 21:35:09 +0800

    build: remove cross targets in make.sh
    Signed-off-by: He Xian <hexian000@outlook.com>

--- make.sh
+++ make.sh
@@ -12,88 +12,25 @@ if git rev-parse --git-dir >/dev/null 2>&1; then
     fi
 fi
 
-if ! [ -d "${OUTDIR}" ]; then
-    OUTDIR="$(realpath ./build)"
-    mkdir -p "${OUTDIR}"
-fi
-
-if [ -z "${OUT}" ]; then
-    OUT="tlswrapper"
-fi
-
 set -e
+OUTDIR="$(realpath ./build)"
 MODROOT="./v3"
 PACKAGE="./cmd/tlswrapper"
-GOFLAGS="${GOFLAGS} -trimpath"
+GOFLAGS="${GOFLAGS} -v -trimpath"
 GCFLAGS="${GCFLAGS}"
 LDFLAGS="-X github.com/hexian000/tlswrapper/v3.Version=${VERSION}"
 
 cd "${MODROOT}"
 go mod vendor
 case "$1" in
-"tlswrapper.android-arm64")
-    GOFLAGS="-buildmode=pie ${GOFLAGS}"
-    LDFLAGS="${LDFLAGS} -s -w"
-    set -x
-    CGO_ENABLED=1 GOOS="android" GOARCH="arm64" \
-        go build ${GOFLAGS} -gcflags "${GCFLAGS}" -ldflags "${LDFLAGS}" \
-        -o "${OUTDIR}/$1" "${PACKAGE}"
-    ;;
-"tlswrapper.linux-amd64")
+"r")
     GOFLAGS="-buildmode=exe ${GOFLAGS}"
     LDFLAGS="${LDFLAGS} -s -w"
     set -x
-    CGO_ENABLED=0 GOOS="linux" GOARCH="amd64" \
+    CGO_ENABLED=0 \
         go build ${GOFLAGS} -gcflags "${GCFLAGS}" -ldflags "${LDFLAGS}" \
-        -o "${OUTDIR}/$1" "${PACKAGE}"
-    ;;
-"tlswrapper.linux-arm64")
-    GOFLAGS="-buildmode=exe ${GOFLAGS}"
-    LDFLAGS="${LDFLAGS} -s -w"
-    set -x
-    CGO_ENABLED=0 GOOS="linux" GOARCH="arm64" \
-        go build ${GOFLAGS} -gcflags "${GCFLAGS}" -ldflags "${LDFLAGS}" \
-        -o "${OUTDIR}/$1" "${PACKAGE}"
-    ;;
-"tlswrapper.linux-armv7")
-    GOFLAGS="-buildmode=exe ${GOFLAGS}"
-    LDFLAGS="${LDFLAGS} -s -w"
-    set -x
-    CGO_ENABLED=0 GOOS="linux" GOARCH="arm" GOARM=7 \
-        go build ${GOFLAGS} -gcflags "${GCFLAGS}" -ldflags "${LDFLAGS}" \
-        -o "${OUTDIR}/$1" "${PACKAGE}"
-    ;;
-"tlswrapper.linux-mipsle")
-    GOFLAGS="-buildmode=exe ${GOFLAGS}"
-    LDFLAGS="${LDFLAGS} -s -w"
-    set -x
-    CGO_ENABLED=0 GOOS="linux" GOARCH="mipsle" GOMIPS="softfloat" \
-        go build ${GOFLAGS} -gcflags "${GCFLAGS}" -ldflags "${LDFLAGS}" \
-        -o "${OUTDIR}/$1" "${PACKAGE}"
-    ;;
-"tlswrapper.windows-amd64.exe")
-    GOFLAGS="-buildmode=pie ${GOFLAGS}"
-    LDFLAGS="${LDFLAGS} -s -w"
-    set -x
-    CGO_ENABLED=1 GOOS="windows" GOARCH="amd64" \
-        go build ${GOFLAGS} -gcflags "${GCFLAGS}" -ldflags "${LDFLAGS}" \
-        -o "${OUTDIR}/$1" "${PACKAGE}"
-    ;;
-"tlswrapper-debug.linux-amd64")
-    GOFLAGS="-buildmode=exe -race ${GOFLAGS}"
-    GCFLAGS="${GCFLAGS} -N -l"
-    set -x
-    CGO_ENABLED=1 GOOS="linux" GOARCH="amd64" \
-        go build ${GOFLAGS} -gcflags "${GCFLAGS}" -ldflags "${LDFLAGS}" \
-        -o "${OUTDIR}/$1" "${PACKAGE}"
-    ;;
-"tlswrapper-debug.linux-arm64")
-    GOFLAGS="-buildmode=exe -race ${GOFLAGS}"
-    GCFLAGS="${GCFLAGS} -N -l"
-    set -x
-    CGO_ENABLED=1 GOOS="linux" GOARCH="arm64" \
-        go build ${GOFLAGS} -gcflags "${GCFLAGS}" -ldflags "${LDFLAGS}" \
-        -o "${OUTDIR}/$1" "${PACKAGE}"
+        -o "${OUTDIR}/" "${PACKAGE}"
+    ls -lh "${OUTDIR}/"
     ;;
 "d")
     GOFLAGS="-buildmode=exe -race ${GOFLAGS}"
@@ -102,8 +39,8 @@ case "$1" in
     go fmt ./... && go mod tidy
     CGO_ENABLED=1 \
         go build ${GOFLAGS} -gcflags "${GCFLAGS}" -ldflags "${LDFLAGS}" \
-        -o "${OUTDIR}/${OUT}" "${PACKAGE}"
-    ls -lh "${OUTDIR}/${OUT}"
+        -o "${OUTDIR}/" "${PACKAGE}"
+    ls -lh "${OUTDIR}/"
     ;;
 "t")
     set -x
@@ -113,7 +50,7 @@ case "$1" in
 *)
     set -x
     go build ${GOFLAGS} -gcflags "${GCFLAGS}" -ldflags "${LDFLAGS}" \
-        -o "${OUTDIR}/${OUT}" "${PACKAGE}"
-    ls -lh "${OUTDIR}/${OUT}"
+        -o "${OUTDIR}/" "${PACKAGE}"
+    ls -lh "${OUTDIR}/"
     ;;
 esac
