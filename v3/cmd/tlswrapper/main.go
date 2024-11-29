b381dd138b16841973ef266dbb2d23d98a979433
He Xian <hexian000@outlook.com>
2024-11-29 19:55:23 +0800

    main: flag aliases
    Signed-off-by: He Xian <hexian000@outlook.com>

--- v3/cmd/tlswrapper/main.go
+++ v3/cmd/tlswrapper/main.go
@@ -2,21 +2,31 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
 
 	"github.com/hexian000/gosnippets/slog"
 	"github.com/hexian000/tlswrapper/v3"
 )
 
+var flagAlias = map[string]string{
+	"help":   "h",
+	"config": "c",
+}
+
 func parseFlags(f *tlswrapper.AppFlags) {
-	flag.BoolVar(&f.Help, "h", false, "help")
-	flag.StringVar(&f.Config, "c", "", "config file")
+	flag.BoolVar(&f.Help, "help", false, "show usage and exit")
+	flag.StringVar(&f.Config, "config", "", "config file")
 	flag.BoolVar(&f.DumpConfig, "dumpconfig", false, "dump config file to stdout")
 	flag.StringVar(&f.ServerName, "sni", "example.com", "server name")
 	flag.StringVar(&f.GenCerts, "gencerts", "", "comma-separated name list, generate key pairs as <name>-cert.pem, <name>-key.pem")
 	flag.StringVar(&f.Sign, "sign", "", "sign the certificate with <name>-cert.pem, <name>-key.pem")
 	flag.StringVar(&f.KeyType, "keytype", "rsa", "one of rsa, ecdsa, ed25519")
 	flag.IntVar(&f.KeySize, "keysize", 0, "specifies the size of the private key, depending on the key type")
+	for from, to := range flagAlias {
+		flagSet := flag.Lookup(from)
+		flag.Var(flagSet.Value, to, fmt.Sprintf("alias to %q", flagSet.Name))
+	}
 	flag.Parse()
 }
 
